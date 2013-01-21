5598f095c721c6fd692c9e486c1b5dd7a1afec26
Matias Linares <matiaslina@gmail.com>
2013-01-21 12:21:02 -0300

    Deleted some debug output

--- gmailApplet.py
+++ gmailApplet.py
@@ -7,21 +7,14 @@ from gi.repository import PanelApplet
 
 def applet_fill(applet):
 	label = Gtk.Label("Hello World")
-	print("label created")
 	applet.add(label)
-	print("label added")
 	applet.show_all()
-	print("applet showed")
 
 ''' Entry point '''
 def applet_factory ( applet, iid, data = None):
 	if iid != "gmail":
-		print("iid = " + iid)
-		print("FALSE")
 		return False
 	
 	applet_fill(applet)
-	print ("applet filled")
-	
-	print("Return true")
+
 	return True
