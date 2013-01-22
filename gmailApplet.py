017c361dbd784ad665228d76268ea34ae3a59b39
Matias Linares <matiaslina@gmail.com>
2013-01-21 22:40:11 -0300

    Added an svg of gmail and changed some names to consort

--- gmailApplet.py
+++ gmailApplet.py
@@ -5,16 +5,33 @@ import gi
 from gi.repository import Gtk
 from gi.repository import PanelApplet
 
-def applet_fill(applet):
-	label = Gtk.Label("Hello World")
-	applet.add(label)
-	applet.show_all()
+class GmailApplet:
+
+	HAVE_ICON = False
+	
+	def __init__(self, applet):
+		self.applet = applet
+		self.button = Gtk.Button()
+
+	def fill(self):
+		try:
+			self.icon = Gtk.Image()
+			self.icon.set_from_file("/usr/share/pixmaps/gmail-applet/update-mail.png")
+			self.button.set_property("image", self.icon)
+			self.HAVE_ICON = True
+		except:
+			self.button.set_label("Gmail")
+		
+		self.applet.set_background_widget(self.button)
+		self.applet.add(self.button)
+		self.applet.show_all()
 
 ''' Entry point '''
 def applet_factory ( applet, iid, data = None):
 	if iid != "gmail":
 		return False
 	
-	applet_fill(applet)
+	gmail = GmailApplet( applet )
+	gmail.fill()
 
 	return True
